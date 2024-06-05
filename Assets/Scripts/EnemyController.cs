16ea48350b78d5194e5cf9e21f0f91edd7aa6c31
hongjaewon62 <hongjaewon0602@gmail.com>
2024-06-05 23:03:54 +0900

    Enemy AI Change

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
@@ -11,22 +12,72 @@ public class EnemyController : MonoBehaviour
     public int attackDamage;                    // 데미지
     public float attackCooldown;                // 데미지 쿨타임
 
+    public Vector3 attackRange;                 // 공격 범위
+    public float attackDistance;                // 공격 거리
+    public Vector3 sightRange;                  // 시야 범위
+    public float sightDistance;                 // 시야 거리
+
+    public LayerMask playerLayer;               // 플레이어 레이어
+
 
     private Animator anim;
+    private BoxCollider boxCollider;
 
+    public NavMeshAgent agent;
+    public float moveRange;
+
+    public Transform centrePoint;
 
     private void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
-
+        boxCollider = GetComponent<BoxCollider>();
         enemyHealth = GetComponent<EnemyHealth>();
-
+        if(gameObject.transform.parent != null)
+        {
+            centrePoint = gameObject.transform.parent.gameObject.transform;
+        }
 
     }
 
     private void Update()
     {
         EnemyAttack();
+        RandomMove();
+    }
+
+    private void RandomMove()
+    {
+        if(agent == null)
+        {
+            return;
+        }
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            Vector3 point;
+            if (RandomPoint(centrePoint.position, moveRange, out point))
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); 
+                agent.SetDestination(point);
+            }
+        }
+    }
+
+    private bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+
+        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
     }
 
     private void EnemyAttack()
@@ -36,9 +87,7 @@ public class EnemyController : MonoBehaviour
 
     public void EnemyDamage()              // 적 데미지
     {
-        GetComponent<EnemyDirector>().SetMovingAnimation(false);
         anim.SetTrigger("Damage");
-
     }
 
     public void EnemyDie()                 // 적 사망
@@ -48,9 +97,8 @@ public class EnemyController : MonoBehaviour
 
     private IEnumerator Attack()
     {
-        if (GetComponent<EnemyDirector>().PlayerInSight())
+        if (PlayerInSight())
         {
-            GetComponent<EnemyDirector>().SetMovingAnimation(false);
             anim.SetTrigger("Attack");
             //TakeDamage();
             yield return new WaitForSeconds(attackCooldown);
@@ -65,5 +113,44 @@ public class EnemyController : MonoBehaviour
         Destroy(gameObject);
     }
 
+    private bool PlayerInSight()
+    {
+        //Vector3 size = new Vector3(boxCollider.bounds.size.x * attackRange.x, boxCollider.bounds.size.y * attackRange.y, boxCollider.bounds.size.z * attackRange.z);
+        //RaycastHit hit;
+        //Physics.BoxCast(transform.position, size, transform.position, out hit, transform.rotation, playerLayer);
+        bool isHit = Physics.CheckBox(transform.position, attackRange, transform.rotation, playerLayer);
 
+        if (isHit)
+        {
+            //playerHealth = hit.transform.GetComponent<Health>();
+            Debug.Log("발견");
+        }
+
+        return isHit;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (boxCollider == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.yellow;
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        Gizmos.DrawWireCube(Vector3.zero, attackRange * 2);
+        //Gizmos.DrawWireCube(transform.position, new Vector3(attackRange.x, attackRange.y, attackRange.z));
+
+        Gizmos.matrix = Matrix4x4.identity;
+        // 공격 범위
+        //Vector3 attackSize = new Vector3(boxCollider.bounds.size.x * attackRange.x, boxCollider.bounds.size.y * attackRange.y, boxCollider.bounds.size.z * attackRange.z);
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawWireCube(boxCollider.bounds.center + transform.forward * attackRange.x * transform.localScale.x * attackDistance, attackSize);
+
+
+        // 시야 범위
+        Vector3 sightSize = new Vector3(boxCollider.bounds.size.x * sightRange.x, boxCollider.bounds.size.y * sightRange.y, boxCollider.bounds.size.z * sightRange.z);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.forward * sightRange.x * transform.localScale.x * sightDistance, sightSize);
+
+    }
 }
